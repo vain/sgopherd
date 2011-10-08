9be85652c4c94b17394847c0e5840e6935193772
Peter Hofmann <scm@uninformativ.de>
2011-10-08 14:11:31 +0200

    Initial draft.

---
+++ bgopherd.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------
+# "THE PIZZA-WARE LICENSE" (Revision 42):
+# Peter Hofmann <pcode@uninformativ.de> wrote this file. As long as you
+# retain this notice you can do whatever you want with this stuff. If
+# we meet some day, and you think this stuff is worth it, you can buy
+# me a pizza in return.
+# ------------------------------------------------------------------
+
+
+# Find and load config.
+config=bgopherd.conf
+
+while getopts c: name; do
+	case $name in
+		c) config="$OPTARG" ;;
+	esac
+done
+
+. "$config" || { echo "Unable to load config file." >&2; exit 1; }
+
+# Library functions.
+rel2abs()
+{
+	[[ -d "$1" ]] && { (cd "$1"; echo "$PWD"); return; }
+	[[ -f "$1" ]] && { (cd "${1%/*}"; echo "$PWD/${1##*/}"); return; }
+}
+
+dirEmpty()
+{
+	set -- "$1"/*
+	[[ ! -e "$1" ]]
+}
+
+parseIndex()
+{
+	sed \
+		-e '/^\[/! { s/.*/i&\t-\t-\t0/ }' \
+		-e '/^\[/ { s/\[//; s/^\(.\)|/\1/; s/\]//; s/|/\t/g }' \
+		-e 's/$/\r/' \
+		"$@"
+}
+
+sendListing()
+{
+	dirEmpty "$1" && return
+
+	for i in "$1"/*; do
+		[[ -d "$i" ]] && itype=1 || itype=0
+
+		printf "%s%s\t%s\t%s\t%s\r\n" \
+			$itype \
+			"${i##*/}" \
+			"${i:${#docroot}}" \
+			"$servername" \
+			"$serverport"
+	done
+}
+
+# Process a request.
+read -r request
+request=${request//$'\r'/}
+absreq=$(rel2abs "$docroot$request")
+if [[ "${absreq:0:${#docroot}}" == "$docroot" ]]; then
+	if [[ -d "$absreq" ]]; then
+		if [[ -f "$absreq"/INDEX ]]; then
+			parseIndex "$absreq"/INDEX
+		else
+			sendListing "$absreq"
+		fi
+	elif [[ -x "$absreq" ]] && [[ "${absreq##*.}" == "cgi" ]]; then
+		"$absreq"
+	elif [[ -x "$absreq" ]] && [[ "${absreq##*.}" == "dcgi" ]]; then
+		"$absreq" | parseIndex
+	else
+		cat "$absreq"
+	fi
+else
+	echo "\`$request' invalid."
+fi
